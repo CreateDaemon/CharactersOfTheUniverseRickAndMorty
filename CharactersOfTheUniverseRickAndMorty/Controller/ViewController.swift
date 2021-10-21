73af275b6da7556f1d4a0e134e1c4087b3e85115
CreateDaemon <dimchik843@gmail.com>
2021-10-21 17:16:19 +0300

    Initial Commit

---
+++ CharactersOfTheUniverseRickAndMorty/Controller/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  CharactersOfTheUniverseRickAndMorty
+//
+//  Created by Дмитрий Межевич on 20.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var character = Character()
+    
+    @IBOutlet var imageCharacter: UIImageView!
+    @IBOutlet var nameCharacter: UILabel!
+    
+    private let characterAPIURL = "https://rickandmortyapi.com/api/character/1"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageCharacter.image = UIImage(named: "RickAndMorty")
+        getDataFromJSON()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameCharacter.text = character.name ?? "No name"
+        
+        getImageCharacter(fromURLString: nil)
+    }
+
+    private func getDataFromJSON() {
+        
+        guard let url = URL(string: characterAPIURL) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            
+            do {
+                self.character = try JSONDecoder().decode(Character.self, from: data)
+            } catch {
+                let error: Error = try! JSONDecoder().decode(Error.self, from: data)
+                // DOTO - print error
+                print(error.error)
+            }
+        }.resume()
+    }
+    
+    private func getImageCharacter(fromURLString urlString: String?) {
+        
+        DispatchQueue.global().async {
+            guard let urlString = urlString,
+                  let url = URL(string: urlString),
+                  let dataURL = try? Data(contentsOf: url)
+            else {
+                return
+            }
+
+            DispatchQueue.main.async {
+                self.imageCharacter.image = UIImage(data: dataURL)
+            }
+        }
+    }
+    
+}
+
---
+++ CharactersOfTheUniverseRickAndMorty/Model/CharactersOfTheUniverse.swift
@@ -0,0 +1,39 @@
+//
+//  CharactersOfTheUniverse.swift
+//  CharactersOfTheUniverseRickAndMorty
+//
+//  Created by Дмитрий Межевич on 20.10.21.
+//
+
+struct Origin: Decodable {
+    
+    var name: String?
+    var url: String?
+}
+
+struct Location: Decodable {
+    
+    var name: String?
+    var url: String?
+}
+
+struct Character: Decodable {
+    
+    var id: Int?
+    var name: String?
+    var status: String?
+    var species: String?
+    var type: String?
+    var gender: String?
+    var origin: Origin?
+    var location: Location?
+    var image: String?
+    var episode: [String]?
+    var url: String?
+    var created: String?
+}
+
+struct Error: Decodable {
+    
+    var error: String
+}
